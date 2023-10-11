b8033e7e1d26e249b82291200e4bb952af513826
helenapaixao <hp.helenapaixao@gmail.com>
2023-10-11 11:27:04 -0300

    feat: add test in input

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Input from "./index";
+
+describe("Input", () => {
+  it("should render the input field correctly", () => {
+    const { getByTestId } = render(<Input data-testid="test-input" />);
+    const inputElement = getByTestId("test-input");
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should trigger the 'onChange' event when text is typed into the input field", () => {
+    const onChange = jest.fn();
+    const { getByTestId } = render(
+      <Input data-testid="test-input" onChange={onChange} />
+    );
+    const inputElement = getByTestId("test-input");
+
+    fireEvent.change(inputElement, { target: { value: "Test Input" } });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+  });
+});
