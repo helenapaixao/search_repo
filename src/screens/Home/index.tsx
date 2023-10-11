da2f4d73383e010f755fa8998c725116f95b5992
helenapaixao <hp.helenapaixao@gmail.com>
2023-10-11 11:15:29 -0300

    feat: add loading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,27 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { searchRepositories, Repository } from "../../services/api";
 import RepositoryDetails from "../../components/RepoDetail";
 import SearchBar from "../../components/Search";
 import RepoList from "../../components/RepoList";
+import Loading from "../../components/Loading";
 
 function Home() {
   const [searchResults, setSearchResults] = useState<Repository[]>([]);
   const [selectedRepository, setSelectedRepository] =
     useState<Repository | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-const [isLoading, setIsLoading] = useState(false); 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleSearch = async (query: string) => {
-     
+    setIsLoading(true);
     try {
       const results = await searchRepositories(query);
       setSearchResults(results);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
     } catch (error) {
       console.error("Erro ao buscar repositórios:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -37,7 +41,7 @@ const [isLoading, setIsLoading] = useState(false);
 
       <div>
         {isLoading ? (
-          <p>Carregando...</p>
+          <Loading />
         ) : searchResults.length > 0 ? (
           <>
             <h2>Resultados da pesquisa:</h2>
@@ -46,8 +50,8 @@ const [isLoading, setIsLoading] = useState(false);
               handleRepositoryClick={handleRepositoryClick}
             />
           </>
-          ) : (
-              <div/>
+        ) : (
+          <div />
         )}
       </div>
       {selectedRepository && (
